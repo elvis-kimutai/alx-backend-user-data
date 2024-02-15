9e1d8e5c89d37e8501fa75e9be2862518295f6e6
elvis-kimutai <cheruiyotkim17@gmail.com>
2024-02-16 00:39:27 +0300

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python3
-"""Session Authentication"""
+"""Session authentication module"""
+import uuid
+from typing import (
+    TypeVar,
+    Union
+)
 from api.v1.auth.auth import Auth
 from models.user import User
-import uuid
 
 
 class SessionAuth(Auth):
-    """Authenticates class using session"""
-
+    """Authenticats class implementation"""
     user_id_by_session_id = {}
 
-    def create_session(self, user_id: str = None) -> str:
-        """creates session_id for a user_id"""
-        if user_id is not None and type(user_id) == str:
-            session_id = str(uuid.uuid4())
-            self.user_id_by_session_id[session_id] = user_id
-            return session_id
-        return None
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """returns User id based on a session_id"""
-        if session_id is not None and type(session_id) == str:
-            user_id = self.user_id_by_session_id.get(session_id)
-            if user_id is not None:
-                return user_id
+    def create_session(self, user_id: str = None) -> Union[str, None]:
+        """ Creates session_id for a user_id """
+        if user_id is None or type(user_id) != str:
             return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id.update({session_id: user_id})
+        return session_id
 
-    def current_user(self, request=None):
-        """Returns a User instance based on a cookie value"""
-        cookie = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(cookie)
-        return User.get(user_id)
+    def user_id_for_session_id(
+        self, session_id: str = None
+    ) -> Union[str, None]:
+        """Get user id from given session id"""
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
 
-    def destroy_session(self, request=None):
-        """Deletes the users session"""
+    def current_user(self, request=None) -> Union[TypeVar('User'), None]:
+        """holds the current authenticated logged in user"""
+        User.load_from_file()
+        return User.get(
+            self.user_id_for_session_id(self.session_cookie(request))
+        )
+
+    def destroy_session(self, request=None) -> bool:
+        """deletes the user session"""
+        if request is None:
+            return False
         session_id = self.session_cookie(request)
         if session_id is None:
             return False
-
         user_id = self.user_id_for_session_id(session_id)
         if user_id is None:
             return False
